ca6f6391d6a39587f3107d4f2c2fe0bd77272d5e
Mednafen <mednafen@users.noreply.github.com>
2024-04-05 12:14:43 -0700

    1.32.1

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
  <p>
  The following systems are supported(refer to the emulation module documentation for more details):
  <ul>
-  <li>Apple II/II+</li>
+  <li>Apple II/II+/IIe/Enhanced IIe</li>
   <li>Atari Lynx</li>
   <li>Neo Geo Pocket (Color)</li>
   <li>WonderSwan</li>
